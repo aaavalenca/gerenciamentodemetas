6ddf72948570d2022988bef5b902000fb33293fe
Mateus Felipe <mfarn@cin.ufpe.br>
2020-10-22 15:44:54 -0300

    adicao de botoes e formulario para cadastro de alunos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,12 +8,13 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+],
+imports: [
+  BrowserModule,
+  AppRoutingModule,
+  FormsModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
