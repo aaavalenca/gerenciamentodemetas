b83b4f9f4794aa8f53fc86903be2952ea3c1e1b3
aaavalenca <aaav@cin.ufpe.br>
2020-10-25 11:41:52 -0300

    adicao do servidor e rotas para os modulos

--- src/app/aluno/aluno.component.ts
+++ src/app/aluno/aluno.component.ts
@@ -18,9 +18,26 @@ export class AlunoComponent implements OnInit {
 
   }
 
-  onFileChange (event) {
+  tratarCsv(csv : string){
+    let data = csv.split('\r');
+    for (let i = 0; i < data[0].length; i++){
+
+    }
 
   }
 
+  onFileChange (event) {
+  //console.log(event);
+  const file = event.srcElement.files[0];
+  const reader = new FileReader();
+  reader.readAsText(file)
+  reader.onload = (e: any)=> {
+    const csv : string = e.target.result;
+    this.tratarCsv(csv);
+    
+  }
+  }
 
-}
+
+
+  }
