e0ccf8790c60d175fff2904e5cfae42555c653ba
Mateus Felipe <mfarn@cin.ufpe.br>
2020-10-24 23:46:25 -0300

    criacao da classe aluno service e modularizacao do app em alunos e metas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 
-import { AlunoService } from './aluno/aluno.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  constructor(private alunoService: AlunoService) {} 
-  aluno: Aluno = {nome: "", cpf: "", email: ""}; 
-  alunos: Aluno[] = [];
-}
 
-export class Aluno {
-  nome: string;
-  cpf: string;
-  email: string;
 }
