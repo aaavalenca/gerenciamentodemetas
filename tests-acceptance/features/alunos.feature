5876e2af1e141d5a44bd99407acfc4b1c3f58f84
aaavalenca <aaav@cin.ufpe.br>
2020-11-02 21:39:47 -0300

    adicao de mais um cenario e teste de alunos

--- tests-acceptance/features/alunos.feature
+++ tests-acceptance/features/alunos.feature
@@ -4,6 +4,20 @@ Feature: As a professor
 
 Scenario: Registering students with spreadsheet
 Given I am at the students page
-And I cannot see a student with CPF "093" in the students list
+And I cant see a student with CPF "093" in the students list
 When I upload a spreadsheet containing my students
 Then I can see a student with CPF "093" in the students list
+
+Scenario: Registering students with spreadsheet when there's already a student registered
+Given I am at the students page
+And I can see a student with CPF "093" in the students list
+And I cannot see a student with CPF "111" in the students list
+When I upload a new spreadsheet containing my students
+Then I can still see a student with CPF "093"
+And I can see a student with CPF "111"
+
+# Scenario: Registering student with registered CPF
+# Given I am at the students page
+# Given I can see a student with CPF "093" in the students list
+# When I try to register the student "Andre" with CPF "093"
+# Then I can see an error message
--- tests-acceptance/stepdefinitions/alunos.ts
+++ tests-acceptance/stepdefinitions/alunos.ts
@@ -8,14 +8,19 @@ let path = require("path");
 
 let sameCPF = ((elem: any, cpf:any) => elem.element(by.name('cpflist')).getText().then((text : string) => text === cpf));
 
+
+
 defineSupportCode(function ({ Given, When, Then }) {
+
+    // 1
+
     Given(/^I am at the students page$/, async () => {
         await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('GerenciamentoDeMetas');
         await $("a[name='alunos']").click();
     })
 
-    Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf:string) => {
+    Given(/^I cant see a student with CPF "(\d*)" in the students list$/, async (cpf:string) => {
         var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
         var samecpfs = allcpfs.filter(elem =>
                                       elem.getText().then(text => text === cpf));
@@ -30,7 +35,47 @@ defineSupportCode(function ({ Given, When, Then }) {
 
     Then(/^I can see a student with CPF "(\d*)" in the students list$/, async (cpf:string) => {
         var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
-        await expect(allalunos.filter(elem => sameCPF(elem,cpf)).then
-                   (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1)));
+        await allalunos.filter(elem => sameCPF(elem,cpf)).then
+                   (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
     });
+
+    // 2
+
+    Given (/^I can see a student with CPF "(\d*)" in the list of students$/, async (cpf:string)=>{
+        await $("input[name='namebox']").sendKeys(<string> "Andre");
+        await $("input[name='cpfbox']").sendKeys(<string> cpf);
+        await $("input[name='emailbox']").sendKeys(<string> "aaav@cin");
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await expect(allalunos.filter(elem => sameCPF(elem,cpf)).then
+        (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1)));
+    });
+
+    Given (/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf:string)=>{
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await expect(allalunos.filter(elem => sameCPF(elem,cpf)).then
+        (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0)));
+    });
+
+    When (/^I upload a new spreadsheet containing my students$/, async ()=>{
+        var fileToUpload = "../planilha2.csv";
+        var absolutePath = path.resolve(__dirname, fileToUpload);
+        await element(by.name("uploadbutton")).sendKeys(absolutePath);
+    });
+
+    Then (/^I can still see a student with CPF "(\d*)"$/, async (cpf:string)=>{
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await expect(allalunos.filter(elem => sameCPF(elem,cpf)).then
+        (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1)));
+    });
+
+    Then (/^I can see a student with CPF "(\d*)"$/, async (cpf:string)=>{
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        await expect(allalunos.filter(elem => sameCPF(elem,cpf)).then
+        (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1)));
+    });
+
+    // 3
+
+    
+
 })
